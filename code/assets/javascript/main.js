3fc8d80a850cff5b6be2712ff73f6d8d307b039a
m.kolasinski <m.kolasinski@poczta.fm>
2018-04-21 23:20:48 +0200

    add form with labels and inputs without function

--- code/assets/javascript/main.js
+++ code/assets/javascript/main.js
@@ -1,6 +1,14 @@
 $(document).ready( function() {
 
-    $(".main_nav ul li").on("click", function() {
+    $("._episode").on("click", function() {
         $(".choice__info").fadeOut();
+        $(".azimuth_part").hide();
+        $(".length_part").show();
+    });
+
+    $("._azimuth").on("click", function() {
+        $(".choice__info").fadeOut();
+        $(".length_part").hide();
+        $(".azimuth_part").show();
     });
 });
