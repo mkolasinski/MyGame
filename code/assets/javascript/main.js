79edfa00b26e03a68c5816cb72c210057bf39f75
m.kolasinski <m.kolasinski@poczta.fm>
2018-05-08 22:11:41 +0200

    calculating of azimuth is OK

--- code/assets/javascript/main.js
+++ code/assets/javascript/main.js
@@ -16,17 +16,17 @@ $(document).ready( function() {
         
         var xP, yP, xK, yK, d1, d2, kD1, kD2, sumKd, length, totalLength;
 
-        xP = parseFloat($("input[name=number1]").val());
-        yP = parseFloat($("input[name=number2]").val());
-        xK = parseFloat($("input[name=number3]").val());
-        yK = parseFloat($("input[name=number4]").val());
+        xP = parseInt($("input[name=number1]").val());
+        yP = parseInt($("input[name=number2]").val());
+        xK = parseInt($("input[name=number3]").val());
+        yK = parseInt($("input[name=number4]").val());
         dX = xK - xP;
         dY = yK - yP;
         kDX = Math.pow(dX, 2);
         kDY = Math.pow(dY,2);
         sumKd = kDX + kDY;
         length = Math.sqrt(sumKd);
-        totalLength = length.toFixed(3);
+        totalLength = parseInt(length.toFixed(3));
 
         $(".total_length_p").text("Długość odcinka");
         $(".length_calculate").text(totalLength);
@@ -34,43 +34,47 @@ $(document).ready( function() {
     }));
     
     if( $("button.button-to-calculate-azimuth").on("click", function() {
-        var xP, yP, xK, yK, dX, dY, az, az2_3;
+        var xP, yP, xK, yK, dX, dY, az, az2_3, az4, part1, part2, part3, part4;
 
-        xP = parseFloat($("input[name=number5]").val());
-        yP = parseFloat($("input[name=number6]").val());
-        xK = parseFloat($("input[name=number7]").val());
-        yK = parseFloat($("input[name=number8]").val());
+        xP = parseInt($("input[name=number5]").val());
+        yP = parseInt($("input[name=number6]").val());
+        xK = parseInt($("input[name=number7]").val());
+        yK = parseInt($("input[name=number8]").val());
         dX = xK - xP;
         dY = yK - yP;
-        az = Math.atan((dY/dX)).toFixed(4);
-
-        $(".total_azimuth_p").text("Azymut wynosi: ");
-        $(".azimuth_calculate").text(az);
-        $(".total_azimuth span.unity").text("grad");
+        az = Math.atan(dY/dX).toFixed(4);
+        az2_3 = (Math.atan(dY/dX) + 200).toFixed(4);
+        az4 = (Math.atan(dY/dX) + 400).toFixed(4);
+        part1 = "Azymut znajduje się w pierwszej ćwiartce"
+        part2 = "Azymut znajduje się w drugiej ćwiartce"
+        part3 = "Azymut znajduje się w trzeciej ćwiartce"
+        part4 = "Azymut znajduje się w czwartej ćwiartce"
 
         if( dX > 0 && dY > 0 ) {
-            var part1 = "Azumyt znajduje się w pierwszej ćwiartce";
+            $(".total_azimuth_p").text("Azymut wynosi: ");
+            $(".azimuth_calculate").text(az);
+            $(".total_azimuth span.unity").text("grad");
             $(".azimuth_info").text(part1);
         }
 
-        else if ( dX < 0 && dY > 0 ) {
-            var part2 = "Azymut znajduje się w drugiej ćwiartce";
-            var az2_3 = az + 200;
+        else if( dX < 0 && dY > 0 ) {
+            $(".total_azimuth_p").text("Azymut wynosi: ");
             $(".azimuth_calculate").text(az2_3);
+            $(".total_azimuth span.unity").text("grad");
             $(".azimuth_info").text(part2);
         }
 
         else if ( dX < 0 && dY < 0 ) {
-            var part3 = "Azymut znajduje się w trzeciej ćwiartce";
-            var az2_3 = az + 200;
+            $(".total_azimuth_p").text("Azymut wynosi: ");
             $(".azimuth_calculate").text(az2_3);
+            $(".total_azimuth span.unity").text("grad");
             $(".azimuth_info").text(part3);
         }
 
         else if ( dX > 0 && dY < 0 ) {
-            var part4 = "Azymut znajduje się w czwartej ćwiartce";
-            var az2_4 = az + 400;
-            $(".azimuth_calculate").text(az2_4);
+            $(".total_azimuth_p").text("Azymut wynosi: ");
+            $(".azimuth_calculate").text(az4);
+            $(".total_azimuth span.unity").text("grad");
             $(".azimuth_info").text(part4);
         }
     }));
